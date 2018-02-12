eece7849bb2f3b9f083d6883fb99385760b86c4f
work <nsikhwar@ubuntu.ubuntu-domain>
2018-02-12 15:52:02 +0200

    intial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+ apiKey: "AIzaSyAcmtYhwTx-74Gf1E1Fd-zrFm0SAhuvEp0",
+    authDomain: "vuejsempman.firebaseapp.com",
+    databaseURL: "https://vuejsempman.firebaseio.com",
+    projectId: "vuejsempman",
+    storageBucket: "vuejsempman.appspot.com",
+    messagingSenderId: "479559985157"
+}
